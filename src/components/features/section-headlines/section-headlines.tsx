5681fd4ebd3a16939bb7327ac7de5d5a79370464
enewton-onshape <enewton@ptc.com>
2024-05-07 14:15:11 -0400

    change to red

--- src/components/features/section-headlines/section-headlines.tsx
+++ src/components/features/section-headlines/section-headlines.tsx
@@ -14,6 +14,7 @@ const useStyles = makeStyles((theme: Theme) => ({
     fontSize: '2.25rem',
     fontWeight: 600,
     lineHeight: 1.083,
+    color: "red",
   },
   subline: {
     fontWeight: 400,
